bfd35c6133e37d16fdc43c2e71d7920576a556f3
lajsan <lajsanminlebaeva@mail.ru>
2025-09-24 20:32:27 -0700

    Login, signUp.html, main.jsp

---
+++ src/main/java/server/LoginServlet.java
@@ -0,0 +1,42 @@
+package server;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "Login", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+    public static final Map<String, String> USERS = new HashMap<>();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("login.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        if (login != null && password != null
+                && USERS.containsKey(login)
+                && USERS.get(login).equals(password)) {
+
+            HttpSession session = req.getSession();
+            session.setAttribute("user", login);
+            session.setMaxInactiveInterval(60 * 60);
+
+            Cookie cookie = new Cookie("user", login);
+            cookie.setMaxAge(24 * 60 * 60);
+            cookie.setPath("/");
+            resp.addCookie(cookie);
+
+            resp.sendRedirect("main.jsp");
+        } else {
+            resp.sendRedirect("/login");
+        }
+    }
+}
---
+++ src/main/java/server/SignUpServlet.java
@@ -0,0 +1,29 @@
+package server;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SignUp", urlPatterns = "/sign_up")
+public class SignUpServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("sign_up.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        if (login != null && password != null && !login.trim().isEmpty() && !password.trim().isEmpty()) {
+            LoginServlet.USERS.put(login.trim(), password.trim());
+            resp.sendRedirect("login.html");
+        } else {
+            resp.sendRedirect("sign_up.html");
+        }
+    }
+}
